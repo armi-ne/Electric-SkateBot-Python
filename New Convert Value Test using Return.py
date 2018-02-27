39fc0350938a6034a60a83443e9a9138254e8ca9
Armin E <36742653+armi-ne@users.noreply.github.com>
2018-02-27 01:37:38 +0000

    Add files via upload

---
+++ New Convert Value Test using Return.py
@@ -0,0 +1,37 @@
+# Take value, seperate values and convert
+
+import string
+
+mph_to_kph = 1.609344
+kph_to_mph = 0.621371192
+
+no_numb = str.maketrans(dict.fromkeys('0123456789'))
+no_lett = str.maketrans(dict.fromkeys(string.ascii_lowercase))
+
+print("The purpose of this code is to convert speed units")
+
+def format_data():
+    inputunit = str(value.translate(no_numb))
+    inputspeed = str(value.translate(no_lett))
+    return inputunit, inputspeed
+
+
+def speed_converter():
+    inputunit, inputspeed = format_data()
+    if inputunit == "kph" and desiredunit == "mph":
+        print("Value: " + inputspeed + ", " + "oUnit: " + inputunit + ", " + "dUnit: " + desiredunit)
+        print(str(int(float(inputspeed) * kph_to_mph)) + "mph")
+    elif inputunit == "mph" and desiredunit == "kph":
+        print("Value: " + inputspeed + ", " + "oUnit: " + inputunit + ", " + "dUnit: " + desiredunit)
+        print(str(int(float(inputspeed) * mph_to_kph)) + "kph")
+    else:
+        print("Please input your values correctly")
+
+
+while True:
+    try:
+        value, to_, desiredunit = input('Input, e.g: 200mph to kph ').split()
+        format_data()
+        speed_converter()
+    except ValueError:
+        value, to_, desiredunit = input('Not valid data, try again! ').split()
