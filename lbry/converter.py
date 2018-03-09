b8a223f01771a47e9b6aca06cb4951dcb92ac410
Armin E <36742653+armi-ne@users.noreply.github.com>
2018-03-09 13:31:09 +0000

    Create converter.py

---
+++ lbry/converter.py
@@ -0,0 +1,25 @@
+conversion_dict = {"MPH": 1.60934, "KPH": 0.621371, "KM": 0.621371,
+"MI": 1.60934, "Wh_km": 0.1, "Wh_mi": 0.0621371, "CM": 0.393701, "INCH": 2.54}
+
+
+def executer(value, original, desired):
+    resulting_value_and_unit = main_converter(value, original, desired)
+    return resulting_value_and_unit
+
+
+def main_converter(value, original, desired):
+    if original == "WH":
+        resultkm = float(value) * conversion_dict["Wh_km"]
+        resultmi = float(value) * conversion_dict["Wh_mi"]
+        # printthis = "{0:.2f}".format(float(value)) + " watt hours should get you: " + "{0:.2f}".format(resultkm) + "KM or " + "{0:.2f}".format(resultmi) + "MI"
+        printthis = "{0:.2f} watt hours should get you: ".format(float(value)) + "{0:.2f}KM or ".format(resultkm) + "{0:.2f}MI".format(resultmi)
+    else:
+        result = float(value) * conversion_dict[original]
+        printthis = "{0:.2f}".format(result) + desired
+    return printthis
+
+
+# Testing to see if code works
+inputvalue, inputunit, to_text, desiredunit = input("please input your values ").split(" ")
+
+print(executer(inputvalue.upper(), inputunit.upper(), desiredunit.upper()))
