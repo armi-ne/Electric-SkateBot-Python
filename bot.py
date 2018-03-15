1b9c37c061ec4cd823952113a86b71fe72a3fc9d
Armin E <36742653+armi-ne@users.noreply.github.com>
2018-03-15 19:56:53 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,6 +102,11 @@ async def battery(ctx, series=None, parallel=None, amphour=None, nominal_volt=No
         embed.add_field(name="Total Watt Hours:", value="{0:.2f}".format(float(total_watthour)) + "wh", inline=False)
         embed.add_field(name="Estimated Ranges:", value="{0:.2f}".format(float(total_range_km)) + "km, or " + "{0:.2f}".format(float(total_range_mi)) + "mi")
         await client.say(embed=embed)
+    elif series == parallel == amphour == nominal_volt is None:
+        embed = discord.Embed(title="Hello %s, here's an explanation of how the +battery command works" % (ctx.message.author.name), color=0xFF0000)
+        embed.add_field(name="Usage:", value="In order to make use of this command you are first required to have 4 pieces of information. 1) Series count. 2) Parallel count. 3) Amp hours per cell. 4) Nominal voltage per cell (for li-ion 3.6 is best)")
+        embed.add_field(name="Command Format:", value="+battery #Series value# #Parallel value# #Amp Hour value# #Nominal Voltage value#")
+        await client.send_message(ctx.message.author, embed=embed)
     elif ((len(series) or len(parallel) or len(amphour) or len(nominal_volt)) > 2):
         embed = discord.Embed(title="Electric SkateBot Battery Calculator", color=0xFF0000)
         embed.add_field(name="Value length error:", value="Sorry but the bot only accepts values from 0-99 for each field.", inline=True)
