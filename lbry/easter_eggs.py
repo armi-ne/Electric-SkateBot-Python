5e6ee19b1e6a8b92d6497ba6ed1a8569a5c63f2d
Armin E <36742653+armi-ne@users.noreply.github.com>
2018-03-15 19:41:36 +0000

    Create easter_eggs.py

---
+++ lbry/easter_eggs.py
@@ -0,0 +1,8 @@
+# Who's your daddy
+whos_your_daddy = ["WHO\'S YOUR DADDY", "WHOS YOUR DADDY", "WHO’S YOUR DADDY", "WHO IS YOUR DADDY"]
+
+# Ben pls
+ben_pls = ["BEN PLS", "BENPLS"]
+
+# Moshi Moshi
+moshi_moshi = ["MOSHI MOSHI"]
