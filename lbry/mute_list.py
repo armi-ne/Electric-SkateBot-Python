46053eab45d0f44c46e0827f6231fef4f8681456
Armin E <36742653+armi-ne@users.noreply.github.com>
2018-03-18 19:51:35 +0000

    Update mute_list.py

--- lbry/mute_list.py
+++ lbry/mute_list.py
@@ -1,3 +1,12 @@
+import time
+starttime = time.time()
+
 muted_users = {
 
 }
+
+
+def write_to_file():
+    file_ = open("/home/armi-ne/Desktop/Coding/Coding Projects/Python/esk8 bot/Bot Spring Cleaning/Spring Cleaning Bot Folder/muted_users.txt", "w")
+    file_.write(str(muted_users))
+    file_.close()
