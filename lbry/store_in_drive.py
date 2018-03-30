5174ff5308670973f1b3d9d07c657a26c5e4943c
Armin E <36742653+armi-ne@users.noreply.github.com>
2018-03-30 14:56:27 +0100

    Update store_in_drive.py

--- lbry/store_in_drive.py
+++ lbry/store_in_drive.py
@@ -5,8 +5,32 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 scope = ["https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/Administrator/Desktop/Electric-SkateBot-Python-Master/client_secret.json", scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/Administrator/Dropbox/Coding/Coding Projects/Python/Electric-SkateBot_Server_Test/client_secret.json", scope)
 client = gspread.authorize(credentials)
+
+def upload_to_drive_new(context, messages):
+    time = datetime.datetime.now()  # Gets the time
+    import_time = time.strftime("%D, %H:%M:%S")  # Formats the time
+    sheet = client.open("ESK8 Server").sheet1
+    deleted_on = ["Deleted on", import_time, "by", context.message.author.name]
+    sheet.insert_row(deleted_on, 1)
+    new_empty_row = ["  ", "  ", "  ", "  "]
+    sheet.insert_row(new_empty_row, 1)
+    timestamps = ""
+    authors = ""
+    contents = ""
+    channels = ""
+    for stuff in messages:
+        timestamps = timestamps + "\n" + str(stuff.timestamp)
+        authors = authors + "\n" + str(stuff.author)
+        contents = contents + "\n" + str(stuff.content)
+        channels = channels + "\n" + str(stuff.channel)        
+    values_to_ins = [str(timestamps), str(authors), str(contents), str(channels)]
+    sheet.insert_row(values_to_ins, 1)
+    timestamps = ""
+    authors = ""
+    contents = ""
+    channels = ""
 
 
 def upload_to_drive(context, messages):
@@ -23,4 +47,4 @@ def upload_to_drive(context, messages):
         value_to_ins = [str(inside.timestamp), str(inside.author), str(inside.content), str(inside.channel)]
         sheet.insert_row(value_to_ins, 1)
     sheet.insert_row(new_empty_row, 1)
-    sheet.insert_row(new_empty_row, 1)
+
